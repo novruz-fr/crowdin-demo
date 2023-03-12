d2219a3a1afdbcf1a240549a2b8ec33ff416e5eb
Novruz Aliyev <aliyevnf@KapMac129-M.local>
2023-03-12 21:17:48 +0400

    first commit

---
+++ src/test/java/az/novruz/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package az.novruz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
